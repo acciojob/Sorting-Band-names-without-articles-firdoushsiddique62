794e2d28be7917e2786f1fe51e9d5a88e8c383ff
FIRDOUSH ALI SIDDIQUE <115089495+firdoushsiddique62@users.noreply.github.com>
2023-08-17 21:30:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 //your code here
+// Given array of band names
+let bandNames = ['The Rolling Stones', 'Aerosmith', 'The Beatles', 'Red Hot Chili Peppers', 'Led Zeppelin'];
 
+// Function to remove articles and sort lexicographically
+function sortBandNames(bandNames) {
+  const articles = ['the', 'a', 'an'];
+
+  // Custom sorting function
+  function customSort(a, b) {
+    const cleanA = a.replace(/^the |^a |^an /i, ''); // Remove article from the beginning
+    const cleanB = b.replace(/^the |^a |^an /i, ''); // Remove article from the beginning
+    return cleanA.localeCompare(cleanB);
+  }
+
+  return bandNames.sort(customSort);
+}
+
+// Sort the band names
+const sortedBandNames = sortBandNames(bandNames);
+
+// Get the ul element to append the list items
+const ul = document.getElementById('band');
+
+// Add sorted band names to the ul element
+sortedBandNames.forEach(bandName => {
+  const li = document.createElement('li');
+  li.textContent = bandName;
+  ul.appendChild(li);
+});
