5da25081515e9016538297bcb976ec7a114f8404
FIRDOUSH ALI SIDDIQUE <115089495+firdoushsiddique62@users.noreply.github.com>
2023-08-17 22:03:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
-//your code here
-// Given array of band names
-let bandNames = ['The Rolling Stones', 'Aerosmith', 'The Beatles', 'Red Hot Chili Peppers', 'Led Zeppelin'];
+ //your code here
 
-// Function to remove articles and sort lexicographically
-function sortBandNames(bandNames) {
-  const articles = ['the', 'a', 'an'];
-
-  // Custom sorting function
-  function customSort(a, b) {
-    const cleanA = a.replace(/^the |^a |^an /i, ''); // Remove article from the beginning
-    const cleanB = b.replace(/^the |^a |^an /i, ''); // Remove article from the beginning
-    return cleanA.localeCompare(cleanB);
-  }
-
-  return bandNames.sort(customSort);
+let bandNames=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
+let articles = ['A', 'AN', 'THE']
+ 
+	bandNames.sort((a,b) => (strip(a)> strip(b))? 1: -1)
+	const ref= document.getElementsByTagName("ul")[0] 
+	for (let i=0;i<bandNames.length; i++){
+		const li = document.createElement("li")
+		li.innerText= bandNames [i]
+		ref.append(li)
+	}
+function strip(word){
+	let arr = word.split(" ")
+	let s=""
+	for(let i=0;i<arr.length;i++){
+if(articles.indexOf(arr[i].toUpperCase()) === -1){
+	s=s+arr[i]
 }
-
-// Sort the band names
-const sortedBandNames = sortBandNames(bandNames);
-
-// Get the ul element to append the list items
-const ul = document.getElementById('band');
-
-// Add sorted band names to the ul element
-sortedBandNames.forEach(bandName => {
-  const li = document.createElement('li');
-  li.textContent = bandName;
-  ul.appendChild(li);
-});
+}
+return s.trim()
+}
